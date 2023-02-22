58e9a76acaf9a9c08e2d167df0a8c5261154e522
Attoungbre Ange François <angeattoungbre21@gmail.com>
2023-02-22 16:52:44 +0000

    design page
    Création de la page de design et affichage des attributs de model

--- src/main/java/angeATT/Tacocloud/domains/Ingredient.java
+++ src/main/java/angeATT/Tacocloud/domains/Ingredient.java
@@ -1,12 +1,16 @@
 package angeATT.Tacocloud.domains;
 
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class Ingredient {
+    @NonNull
     private final String id;
+    @NonNull
     private final String name;
-    private final String type;
+    @NonNull
+    private final Type type;
 
     public enum Type{
         WRAP,PROTEIN,VEGGIES,CHEESE,SAUCE;
--- src/main/java/angeATT/Tacocloud/domains/Taco.java
+++ src/main/java/angeATT/Tacocloud/domains/Taco.java
@@ -2,11 +2,13 @@ package angeATT.Tacocloud.domains;
 
 
 import lombok.Data;
+import lombok.NonNull;
 
 import java.util.List;
 
 @Data
 public class Taco {
+    @NonNull()
     private String name;
     private List<Ingredient> ingredients;
 }
