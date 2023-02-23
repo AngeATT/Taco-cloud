af98b9803dc4e756ffde1bd0b397650362cd25f1
Attoungbre Ange François <angeattoungbre21@gmail.com>
2023-02-23 15:57:41 +0000

    persistence for domains and yaml creation

--- src/main/java/angeATT/Tacocloud/domains/Taco.java
+++ src/main/java/angeATT/Tacocloud/domains/Taco.java
@@ -5,14 +5,19 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class Taco {
+
+    private Long id; //for data persistence
+
+    private Date createdAt = new Date(); //date de création pour la persistance
     @NotNull
-    @Size(min=5,message ="Entrez un nom d'au moins 5 caractères")
+    @Size(min = 5, message = "Entrez un nom d'au moins 5 caractères")
     private String name;
     @NotNull
-    @Size(min=1,message ="Choississez au moins un ingrédient")
+    @Size(min = 1, message = "Choississez au moins un ingrédient")
     private List<Ingredient> ingredients;
 }
