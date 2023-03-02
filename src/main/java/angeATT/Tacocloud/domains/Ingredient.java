8017ea545fea5e01c662e45299cde70a868bfe26
Attoungbre Ange François <angeattoungbre21@gmail.com>
2023-03-02 09:14:37 +0000

    Attribute converter
    AJout d'attributs converter pour régler le problème de conversion type en string

--- src/main/java/angeATT/Tacocloud/domains/Ingredient.java
+++ src/main/java/angeATT/Tacocloud/domains/Ingredient.java
@@ -1,5 +1,7 @@
 package angeATT.Tacocloud.domains;
 
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.*;
@@ -12,9 +14,24 @@ public class Ingredient {
     @Id
     private final String id;
     private final String name;
+    @Convert(converter = TypeToStringConverter.class)
     private final Type type;
 
     public enum Type{
         WRAP,PROTEIN,VEGGIES,CHEESE,SAUCE;
     }
+
+    public static class TypeToStringConverter implements AttributeConverter<Type,String>{
+
+        @Override
+        public String convertToDatabaseColumn(Type type) {
+            return type.toString();
+        }
+
+        @Override
+        public Type convertToEntityAttribute(String s) {
+            return Type.valueOf(s);
+        }
+    }
+
 }
