c914eaed4b0164a3f261d48b91d8a2847ea1cf66
Attoungbre Ange François <angeattoungbre21@gmail.com>
2023-03-02 12:48:49 +0000

    ajout de spring security

--- src/main/java/angeATT/Tacocloud/repositories/OrderRepository.java
+++ src/main/java/angeATT/Tacocloud/repositories/OrderRepository.java
@@ -4,6 +4,13 @@ import angeATT.Tacocloud.domains.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+
+
+    List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+
 }
