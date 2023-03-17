0fdbda45b4407d6d290c98e919f2c7c9bc9a8bca
AngeATT <96740251+AngeATT@users.noreply.github.com>
2023-03-17 19:50:13 +0000

    Ajout effective de Bootstrap et des outils d'automatisation pour le css -Configuration de gulp pour utiliser des plugins et postcss -Configuration du pom xml pour build automatiquement

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp = require('gulp');
+const rename = require("gulp-rename");
 const babel = require("gulp-babel");
 const watch = require('gulp-watch');
 const browserSync = require('browser-sync').create();
@@ -8,6 +9,9 @@ const terser = require('gulp-terser');
 const postcss = require('gulp-postcss');
 const purgecss = require('gulp-purgecss');
 const production = environments.production;
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
 gulp.task('watch', () => {
   browserSync.init({
   proxy: 'localhost:8080',
@@ -20,21 +24,28 @@ gulp.task('copy-html', () =>
   gulp.src(['src/main/resources/**/*.html'])
   .pipe(gulp.dest('target/classes/'))
 );
+
 gulp.task('copy-css', () =>
   gulp.src(['src/main/resources/**/*.css'])
   .pipe(postcss())
   .pipe(production(uglifycss()))
+  .pipe( purgecss({
+   content: ['src/main/resources/**/*.html']}))
+  .pipe(cleanCSS())
   .pipe(gulp.dest('target/classes/'))
 );
+
 gulp.task('copy-js', () =>
   gulp.src(['src/main/resources/**/*.js'])
   .pipe(babel())
   .pipe(production(terser()))
   .pipe(gulp.dest('target/classes/'))
 );
+
 gulp.task('copy-html-and-reload', gulp.series('copy-html', reload));
 gulp.task('copy-css-and-reload', gulp.series('copy-css', reload));
 gulp.task('copy-js-and-reload', gulp.series('copy-js', reload));
+
 gulp.task('build', gulp.series('copy-html', 'copy-css', 'copy-js'));
 gulp.task('default', gulp.series('watch'));
 
