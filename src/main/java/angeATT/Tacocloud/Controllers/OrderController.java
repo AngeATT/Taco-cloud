638fc558839a262dec2f15a38cf2e51cf7b0f1b0
AngeATT <96740251+AngeATT@users.noreply.github.com>
2023-03-19 19:02:17 +0000

    Sauvegarder les commanandes chez l'utilisateur qui a passé la commande

--- src/main/java/angeATT/Tacocloud/Controllers/OrderController.java
+++ src/main/java/angeATT/Tacocloud/Controllers/OrderController.java
@@ -2,9 +2,11 @@ package angeATT.Tacocloud.Controllers;
 
 import angeATT.Tacocloud.domains.TacoOrder;
 import angeATT.Tacocloud.repositories.OrderRepository;
+import angeATT.Tacocloud.sec.Utilisateur;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,11 +32,13 @@ public class OrderController {
     }
     @PostMapping
     public String processOrder(@Valid TacoOrder order, Errors errors,
-                               SessionStatus sessionStatus) {
+                               SessionStatus sessionStatus,Authentication authentication) {
         if ( errors.hasErrors() ){
             return "orderForm";
         }
+        order.setUser( (Utilisateur) authentication.getPrincipal());
         orderRepository.save(order);
+
         log.info("Order submitted: {}", order);
         sessionStatus.setComplete();
         return "redirect:/";
